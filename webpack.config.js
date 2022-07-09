6b791ede0c15d8bc9b1f064b01cc5b2408c23a58
Jahiker Rojas <52946797+Jahiker@users.noreply.github.com>
2022-07-08 23:52:03 -0400

    [add] service worker

--- webpack.config.js
+++ webpack.config.js
@@ -1,6 +1,7 @@
 const HtmlWebpackPlugin = require("html-webpack-plugin");
 const WebpackPwaManifestPlugin = require("webpack-pwa-manifest");
 const path = require("path");
+const WorkboxWebpackPlugin = require('workbox-webpack-plugin');
 
 module.exports = {
   output: {
@@ -13,18 +14,66 @@ module.exports = {
       template: "src/index.html",
     }),
     new WebpackPwaManifestPlugin({
-      name: "Petgram - Tu app de ftoos de mascotas",
+      name: "Petgram - Tu app de fotos de mascotas",
       shortname: "Petgram 🐶",
       description: "Con Petgram puedes encontrar fotos de animales domésticos my fácilmente",
+      orientation: 'portrait',
+      display: 'standalone',
+      start_url: '/home',
+      scope: '/',
       background_color: "#fff",
       theme_color: "#b1a",
       icons: [
         {
-          src: path.resolve("src/assets/icon.png"),
-          sizes: [96, 128, 192, 256, 384, 512],
+          src: path.resolve('src/assets/maskable_icon_x128.png'),
+          size: '192x192',
+          purpose: 'any maskable',
+          type: 'image/png'
+        },
+        {
+          src: path.resolve('src/assets/maskable_icon_x192.png'),
+          size: '256x256',
+          purpose: 'any maskable',
+          type: 'image/png'
+        },
+        {
+          src: path.resolve('src/assets/maskable_icon_x384.png'),
+          size: '384x384',
+          purpose: 'any maskable',
+          type: 'image/png'
+        },
+        {
+          src: path.resolve('src/assets/maskable_icon_x512.png'),
+          size: '512x512',
+          purpose: 'any maskable',
+          type: 'image/png'
+        },
+        {
+          src: path.resolve('src/assets/icon.png'),
+          size: '1024x1024',
+          purpose: 'any maskable',
+          type: 'image/png'
         },
       ],
     }),
+    new WorkboxWebpackPlugin.GenerateSW({
+      runtimeCaching: [
+        {
+          urlPattern: new RegExp('https://petgram-server-nine-phi.vercel.app'),
+          handler: 'CacheFirst',
+          options: {
+            cacheName: 'images'
+          }
+        },
+        {
+          urlPattern: new RegExp('https://'),
+          handler: 'NetworkFirst',
+          options: {
+            cacheName: 'api'
+          }
+        }
+      ]
+    })
   ],
   module: {
     rules: [
